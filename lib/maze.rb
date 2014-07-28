73ae19a5e21adbb64048da9817755501500c3eb6
shozo hatta <hachi8833@gmail.com>
2014-07-28 21:12:34 +0900

    prepare_mazeを実装中

--- lib/maze.rb
+++ lib/maze.rb
@@ -12,143 +12,52 @@
 # maze = Maze.new(x, y)
 # maze.plotmaze
 # maze.output
-class Maze
 
-  # 初期値x, y。正の奇数かつ7以上にすること
-  attr_accessor :x, :y
-  # 作成された迷路が保存されている2次元配列
-  attr_accessor :array_2d
-  ROAD, WALL, PREWALL = nil, 1, 2
-  # 表示用文字
+class Point
+  attr_accessor :point
 
-  CHARACTERS = { ROAD => '　', WALL => '〓', PREWALL => '〓' }.freeze
-  # 初期化 (prepare_arrayとinit_arrayもこの中から呼ばれている)
-  def initialize(x, y)
-    # 迷路の外側の壁の厚さ
-    @margin = 1.freeze
-    # 迷路作成上のオフセット値 
-    @offset = 3.freeze
-    # 迷路作成上の増分値
-
-    @increment   = 2.freeze
-    @x, @y = x.freeze, y.freeze
-    @array_2d = prepare_array(x, y)
-    init_array(x, y)
+  def initialize(char)
+    @point = char 
   end
 
-  # 座標にあるものを調べる
-  def getpoint(x, y)
-    @array_2d[x][y]
-  end
-
-  # 座標に壁などを設置
-  def setpoint(x, y, piece)
-    @array_2d[x][y] = piece
-  end
-
-  # 壁を1つ作成、伸ばせるところまで伸ばす
-  def maze_sub(x, y)
-    rnd = rand(0..3)
-    pol = 1 # 回転の向き
-
-    4.times do |cnt| # 方向転換の回数
-      case (rnd + (cnt * pol) + 4) % 4
-      when 0
-        px, py = 0, -1
-      when 1
-        px, py = -1, 0
-      when 2
-        px, py = 0, 1
-      when 3
-        px, py = 1, 0
-      end
-
-      # 壁にぶつかったら壁までつないで戻る
-      if getpoint(x + (px * 2), y + (py * 2)) == WALL
-        setpoint(x, y, WALL)
-        setpoint(x + px, y + py, WALL) 
-        return true 
-      end
-
-      # 行き先が道なら「作りかけの壁」をその方向に伸ばす
-      if getpoint(x + (px * 2), y + (py * 2)) == ROAD
-        setpoint(x, y, PREWALL)
-        setpoint(x + px, y + py, PREWALL) 
-
-        #再帰
-        if maze_sub(x + (px * 2), y + (py * 2))
-          #戻った時に「作りかけの壁」を完成した壁に変える
-          setpoint(x, y, WALL)
-          setpoint(x + px, y + py, WALL)
-          return true
-        else
-          #エラーで戻ったら「作りかけの壁」を道に戻す
-          setpoint(x, y, ROAD)
-          setpoint(x + px, y + py, ROAD)
-          return false
-        end 
-      end
-    end
-    #4方向とも「作りかけの壁」ならエラーで戻る
-    return false
-  end
-
-  # 壁の開始地点をトラバースしてmaze_subを呼び続ける
-  def plotmaze
-    @offset.step(@y - 3, @increment) do |y|
-      @offset.step(@x - 3, @increment) do |x|
-        if getpoint(x, y) == ROAD
-          result = maze_sub(x, y)
-          next if result
-        end
-      end 
-    end
-  end
-
-  # array_2dを整形して出力
   def output
-    @array_2d.each do |line|
-      puts line.map { |c| CHARACTERS[c] }.join
-    end
+    print @point
+  end
+end
+
+class Maze
+  attr_accessor :x, :y, :array_2d
+  attr_reader :road, :wall, :prewall
+
+  def initialize(x, y)
+    @x = x.freeze
+    @y = y.freeze
+    @road, @wall, @prewall = Point.new('　'),
+                             Point.new('〓'),
+                             Point.new('〓')
+    @depth, @offset        = 1.freeze,
+                             2.freeze
+    @array_2d = prepare_maze(@x, @y)
   end
 
   private
 
-  # 配列確保
-  def prepare_array(x, y)
-    array_2d, tmp = [], []
+  def prepare_maze(x, y)
+    array, line = [], []
 
     #最初の行
-    array_2d << [WALL] * y
+    array << [@wall] * y
 
     #途中の行
-    tmp << WALL
-    (y - 2).times { tmp << ROAD }
-    tmp << WALL
-    (x - 2).times {array_2d << tmp.dup }
+    line << @wall
+    (y - (@depth * 2)).times { line << @road }
+    line << @wall
+    (x - (@depth * 2)).times { array << line.dup }
 
     #最後の行
-    array_2d << [WALL] * y
-    return array_2d
-  end
+    array << [@wall] * y
 
-  # 配列内容を初期化
-  # 最外周は道になるので、外側の壁は1つ内側になる
-  def init_array(x, y)
-    x.times do |xx|
-      y.times do |yy|
-        setpoint(xx, yy, ROAD)
-      end
-    end
-
-    (@margin..(@x - 2)).each do |i|
-      setpoint(i, 1, WALL)
-      setpoint(i, @y - 2, WALL)
-    end 
-
-    (@margin..(@y - 2)).each do |j|
-      setpoint(1, j, WALL)
-      setpoint(@x - 2, j, WALL)
-    end 
+    return array
   end
 end
+
--- spec/maze_spec.rb
+++ spec/maze_spec.rb
@@ -4,70 +4,47 @@
 require 'spec_helper'
 # require 'rspec'
 require 'maze'
+require 'pry'
+
+describe :Point do
+  it "インスタンスごとに異なる文字を出力" do
+    maze = Maze.new(7, 3)
+    binding.pry
+    expect(maze.road.output).to eq '　'
+    expect(maze.wall.output).to eq '〓'
+    expect(maze.prewall.output).to eq '〓'
+  end
+end
 
 describe :Maze do
 
   it "引数なしのインスタンス作成は失敗する" do
-    expect{Maze.new}.to raise_error(ArgumentError)
   end
 
   context "インスタンス作成" do
     before :each do
-      @maze = Maze.new(7, 7)
     end
 
     it "インスタンスを作成できる" do
-      expect(@maze.x).to eq 7
-      expect(@maze.y).to eq 7
     end
 
     it "配列が正常に作成できる" do
-      expect(@maze.array_2d).to eq [[nil, nil, nil, nil, nil, nil, nil], [nil, 1, 1, 1, 1, 1, nil], [nil, 1, nil, nil, nil, 1, nil], [nil, 1, nil, nil, nil, 1, nil], [nil, 1, nil, nil, nil, 1, nil], [nil, 1, 1, 1, 1, 1, nil], [nil, nil, nil, nil, nil, nil, nil]] 
     end
   end
 
   context "メソッドの単体テスト" do
-    before :each do @maze = Maze.new(5, 5) end
-
-    # it :prepare_ary do
-    #   @maze.prepare_ary(5, 5)
-    #   expect(@maze.array_2d).to eq [[nil, nil, nil, nil, nil], [nil, 1, 1, 1, nil], [nil, 1, nil, 1, nil], [nil, 1, 1, 1, nil], [nil, nil, nil, nil, nil]] 
-
-    #   @maze = Maze.new(3, 3)
-    #   @maze.prepare_ary(3, 3)
-    #   expect(@maze.array_2d).to eq [[nil, nil, nil], [nil, 1, nil], [nil, nil, nil]]
-    # end
+    before :each do end
 
     it :init_ary do
-      expect(@maze.array_2d).to eq [[nil, nil, nil, nil, nil], [nil, 1, 1, 1, nil], [nil, 1, nil, 1, nil], [nil, 1, 1, 1, nil], [nil, nil, nil, nil, nil]]
     end
 
     it :getpoint do
-      expect(@maze.getpoint(2, 3)).to eq 1
-      expect(@maze.getpoint(2, 2)).to eq nil
-      expect{@maze.getpoint(6, 6)}.to raise_error(NoMethodError)
     end
 
     it :setpoint do
-      @maze.setpoint(2, 3, nil)
-      expect(@maze.getpoint(2, 3)).to eq nil
-      @maze.setpoint(2, 3, 1)
-      expect(@maze.getpoint(2, 3)).to eq 1
-
-      @maze.setpoint(3, 2, nil)
-      expect(@maze.getpoint(3, 2)).to eq nil
-      @maze.setpoint(3, 2, 1)
-      expect(@maze.getpoint(3, 2)).to eq 1
-
-      expect{@maze.setpoint(6, 6, 1)}.to raise_error(NoMethodError)
     end
   end
 
   context :plotmaze do
-    @maze = Maze.new(9, 9)
-    # ペンディング: RSpec 3.0でmaze_subをどうやってスタブ化する?
-    # allow(@maze).to receive(:maze_sub) { true }
-    # @maze.plotmaze.stub(maze_sub: true)
-    #allow(@maze.plotmaze).receive(:maze_sub) { true }
   end
 end
