39bf72588ca30224551f2f49b8250a35367e7f6e
shozo hatta <hachi8833@gmail.com>
2014-07-31 09:34:13 +0900

    plot_mazeを実装

--- lib/maze.rb
+++ lib/maze.rb
@@ -15,6 +15,7 @@ require 'pry'
 # maze.plotmaze
 # maze.output
 
+# 壁/床の種別を表すクラス。@ouptputをポリモーフィックに使用する
 class Point
   attr_reader :output
 
@@ -24,28 +25,33 @@ class Point
 
 end
 
+#メインのクラス
 class Maze
   attr_accessor :x, :y, :array_2d
   attr_reader :road, :wall, :prewall
 
+  #初期化
   def initialize(x, y)
     @x = x.freeze
     @y = y.freeze
     @road, @wall, @prewall = Point.new('　'),
                              Point.new('〓'),
-                             Point.new('〓')
+                             Point.new('$$')
     @depth, @offset        = 1, 2
-    @array_2d = prepare_maze(@x, @y)
+    @array_2d = initialize_array(@x, @y)
   end
 
+  #配列の点を取得
   def get_point(x, y)
-    return @array_2d[x][y].output 
+    return @array_2d[x][y]
   end
 
+  #配列に点を設定
   def set_point(x, y, point)
     @array_2d[x][y] = point
   end
 
+  #配列を出力
   def output
     @array_2d.each do |line|
       # このc.outputはポリモーフィックになっているか?
@@ -53,17 +59,57 @@ class Maze
     end
   end
 
+  #壁を1つ作成可能なところまでランダムに伸ばす
   def plot_maze(x, y)
-    true
+    rnd = rand(0..3) #最初の方向をランダムに決める
+
+    direction = [0, 1, 2, 3]
+    4.times do |i|
+      case direction[rnd - i] #4方向を順繰りに処理
+      when 0
+        dx, dy = 0, -1
+      when 1
+        dx, dy = -1, 0
+      when 2
+        dx, dy = 0, 1
+      when 3
+        dx, dy = 1, 0
+      end
+    
+      case get_point(x + (dx * @offset), y + (dy * @offset))
+      when @wall
+        # 壁にぶつかったら壁までつないで戻る
+        set_point(x, y, @wall)
+        set_point(x + dx, y + dy, @wall)
+        return true
+      when @road
+        # 行き先が道ならprewallをその方向に伸ばす
+        set_point(x, y, @prewall)
+        set_point(x + dx, y + dy, @prewall)
+
+        # 再帰
+        if plot_maze(x + (dx * @offset), y + (dy * @offset))
+          # 戻る時にprewallをwallに変更
+          set_point(x, y, @wall)
+          set_point(x + dx, y + dy, @wall)
+          return true
+        else
+          # エラーで戻ったらprewallをroadに戻してやり直す
+          set_point(x, y, @road)
+          set_point(x + dx, y + dy, @road)
+          return false
+        end
+      end
+    end
   end
 
+  #配列の偶数点をトラバースしてplot_mazeを呼び出す
   def build_maze
-    binding.pry
     even_x = (@offset..(@x - @offset)).select { |dx| dx % 2 == 0 }
     even_y = (@offset..(@y - @offset)).select { |dy| dy % 2 == 0 }
     even_y.each do |y|
       even_x.each do |x|
-        next if get_point(x, y) == @wall.output
+        next if get_point(x, y) == @wall
         until plot_maze(x, y)
         end
       end
@@ -72,7 +118,8 @@ class Maze
 
   private
 
-  def prepare_maze(x, y)
+  #配列の初期化
+  def initialize_array(x, y)
     array, line = [], []
 
     #最初の行
